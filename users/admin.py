0551da2b2902800025d7ed947e8a6a54972dcaf9
GuyMonagan <werevalkury@gmail.com>
2026-02-08 22:20:43 +0400

    нициализация проекта, создан основной конфиг и приложение users

---
+++ users/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    model = User
+    ordering = ('email',)
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_joined')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Персональная информация'), {'fields': ('first_name', 'last_name')}),
+        (_('Права доступа'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Важные даты'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
+    )
+
+    add_form_template = 'admin/auth/user/add_form.html'
+
